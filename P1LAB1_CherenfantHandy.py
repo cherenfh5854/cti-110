f15853b05daf03ec3f5590ef4c1d848c4b9a68e2
cherenfh5854 <86271396+cherenfh5854@users.noreply.github.com>
2024-10-14 14:12:50 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_CherenfantHandy.py
@@ -0,0 +1,11 @@
+#Handy Cherenfant
+#10/13/2024
+#P1LAB1
+#Intro to python
+
+firstName = 'Cynthia'
+lastName = 'Cupcakes'
+print('Enter your first name:',firstName)
+print('Enter your last name:',lastName)
+print('Hello,',firstName, lastName,'! Welcome to CTI-110')
+
