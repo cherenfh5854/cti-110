d30de47b6ff396e2ec980aa806073c3f5a58fc53
cherenfh5854 <86271396+cherenfh5854@users.noreply.github.com>
2024-10-14 14:31:36 -0400

    Add files via upload
    P1HW1

---
+++ P1HW1_CherenfantHandy.py
@@ -0,0 +1,22 @@
+# Handy Cherenfant
+# 10/14/2024
+# P1HW1 Instructions
+# P1HW1_Cherenfant Handy 
+print("-----Calculating Exponents-----")
+print("")
+print("")
+x =int(input("Enter an integer as the base value: ")) 
+y =int(input("Enter an integer as the base exponent: ")) 
+ans= (x ** y)
+print(x, "raised to the power of", y, "is" , ans, "!!")
+print("")
+print("")
+print("-----Adition and Subtraction-----")
+print("")
+print("")
+z =int(input("Enter a starting integer: "))
+c =int(input("Enter an integer to add: "))
+v =int(input("Enter an integer to subtract: "))
+an= ( z+c-v)
+print("")
+print(z," + ",c, " - " ,v , "is equal to" ,an) 
